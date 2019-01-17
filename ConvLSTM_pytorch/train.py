d266961af2d686a0bdba0f0910c3b6e9d5ad3418
Sasha Puchko <alexandra.puchko@gmail.com>
2019-01-17 15:31:26 -0800

    train

--- ConvLSTM_pytorch/train.py
+++ ConvLSTM_pytorch/train.py
@@ -146,7 +146,7 @@ def trainNet(net, loss, optimizer,train_seqs, dev_seqs, test_seqs,args, device, 
                 #update hidden_state to next sequence
                 hidden_states = prev_hidden_states
                 train_loss = loss(train_outputs, mb_y)
-                print("Train loss = %.7f, Year %d" % (train_loss.item(), (mb_row + 1)))
+                print("Train loss = %.7f" % train_loss.item())
                 train_loss.backward()
                 optimizer.step()
 
